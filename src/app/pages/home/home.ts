862a4553d211c57f9d5b4e441e1ddd0b5b2fa051
Abraham <acv81505@lasallebajio.edu.mx>
2025-12-07 15:56:43 -0600

    Conexiión exitosa con Back v1

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router'; // <-- CRÍTICO: Agregamos RouterModule para las directivas routerLink
+import { RouterModule } from '@angular/router';
 import { LibroService } from '../../services/libro';
 import { CategoriaService } from '../../services/categoria';
 import { Libro } from '../../models/book.model';
-import { Categoria } from '../../models/category.model'; // <-- Usamos el nombre del modelo acordado
+import { Categoria } from '../../models/category.model';
 import { LibroCard } from '../libro-card/libro-card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  // CRÍTICO: Agregamos RouterModule aquí
   imports: [CommonModule, RouterModule, LibroCard],
   templateUrl: './home.html',
   styleUrl: './home.scss'
@@ -28,19 +27,15 @@ export class Home implements OnInit {
   }
 
   cargarDatos() {
-    // Carga de Libros Destacados
     this.libroService.getLibros().subscribe({
       next: (data) => {
-        // Tomamos los primeros 4 libros
         this.librosDestacados = data.slice(0, 4);
       },
       error: (err) => console.error('Error cargando libros:', err)
     });
 
-    // Carga de Categorías
     this.categoriaService.getCategorias().subscribe({
       next: (data) => {
-        // Tomamos las primeras 4 categorías para la vista de Home
         this.categorias = data.slice(0, 4);
       },
       error: (err) => console.error('Error cargando categorías:', err)
--- src/app/services/categoria.ts
+++ src/app/services/categoria.ts
@@ -1,14 +1,13 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Categoria } from '../models/category.model'; // Asegúrate de que el path sea correcto
+import { Categoria } from '../models/category.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriaService {
   private http = inject(HttpClient);
-  // Endpoint de tu backend: GET http://localhost:3000/categories
   private apiUrl = 'http://localhost:3000/categories';
 
   getCategorias(): Observable<Categoria[]> {
