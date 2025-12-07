57d3dca7c0bc653c6315d05151b82e68532625af
Abraham <acv81505@lasallebajio.edu.mx>
2025-12-07 12:04:51 -0600

    Separación de carpetas FrontEnd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Catalogo } from './pages/catalogo/catalogo';
+import { Reservar } from './pages/reservar/reservar';
+import { Reservaciones } from './pages/reservaciones/reservaciones';
+import { BookForm } from './pages/book-form/book-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'catalogo', component: Catalogo },
+  { path: 'reservar', component: Reservar },
+  { path: 'mis-reservaciones', component: Reservaciones },
+  { path: 'admin/nuevo-libro', component: BookForm },
+  { path: '**', redirectTo: '' }
+];
